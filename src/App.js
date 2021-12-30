246fc4bf9110ff8c8128d8e9fd0961a40d0b429a
Abdirizak Omer <abdirizakomer188@gmail.com>
2021-12-30 18:05:11 +0300

    Wax Cusub

--- src/App.js
+++ src/App.js
@@ -1,15 +1,79 @@
-
 import Test from "./Component/Test";
 
 function App() {
+  const store = [
+    {
+      name: "Apple",
+      image: "/Apples.jpeg",
+      description: "Organic Granny Apples 3 Lb Pouch",
+
+      quantity: 15,
+      price: 4.96,
+    },
+    {
+      name: "Bananas",
+      image: "/images/Bananas.jpeg",
+      description: "Organic Bananas, Bunch",
+      quantity: 17,
+      price: 1.63,
+    },
+    {
+      name: "Grapes",
+      image: "/images/Grapes.jpeg",
+      quantity: 51,
+      price: 5.98,
+    },
+    {
+      name: "Strawberries",
+      image: "/images/Strawberries.jpeg",
+      description: "Fresh USDA Organic Strawberries, 16 Oz",
+      quantity: 105,
+      price: 4.98,
+    },
+    {
+      name: "Carrots",
+      image: "./Carrots.jpeg",
+      description: "Organic Whole Carrots, 2 Lb Bag",
+      quantity: 92,
+      price: 2.06,
+    },
+    {
+      name: "Tomatoes",
+      image: "./tomatoes.jpeg",
+      description: "Organic Grape Tomatoes, 10 oz",
+      quantity: 26,
+      price: 2.92,
+    },
+    {
+      name: "Avocados",
+      image: "/images/avocados.jpeg",
+      description: "Organic Medium Hass Avocados, 3-5 Count Bag",
+      quantity: 66,
+      price: 4.96,
+    },
+  ];
+
+  const name = "Strawberry";
+  const discription = "Fresh Strawberry Fruit";
+  const Quantity = 25;
+  const price = 39;
+  const ImageUrl = "/strawberries.jpeg";
   return (
     <>
-    <Test/>
-    <Test/>
-    <Test/>
-    <Test/>
-    
-
+      <Test
+        nasme={store[5].name}
+        discription={store[5].description}
+        quantity={store[5].quantity}
+        price={store[5].price}
+        ImageUrl={store[5].image}
+      />
+       <Test
+        nasme={store[4].name}
+        discription={store[4].description}
+        quantity={store[4].quantity}
+        price={store[4].price}
+        ImageUrl={store[4].image}
+      />
     </>
   );
 }
--- src/Component/Test.jsx
+++ src/Component/Test.jsx
@@ -1,19 +1,26 @@
-const Test=  () => {
-    return(
-        <div className="my-5 mx-auto xy-5 border-2 px-2 py-1 max-w-sm rounded-xl shadow-md  my-4 text-center flex flex-col items-center  relative">
-             <div>
-                 <img src="strawberries.jpeg" alt="" className="h-auto w-60"/>
-            <p>Frozen Fresh Strawberry Fruit </p>
-            </div>
-            <span className="bg-red-500 px-2 py-1 rounded-xl text-white absolute top-3 right-5">$39</span>
+const Test = (props) => {
+  return (
+    <div className="my-6 x-5 border-2 px-2 py-1 max-w-sm rounded-xl shadow-md  my-4 text-center flex flex-col   relative">
+      <div>
+        <img src={props.ImageUrl} alt="" className="h-auto w-60" />
+        <h1 className="font-bold text-2xl text-black">{props.name}</h1>
+        <h1 className="bg-red-500 px-2 py-1 rounded-xl text-white inline">
+         quantity {props.quantity}
+        </h1>
+        <p> {props.discription} </p>
+      </div>
+      <div></div>
+      <span className="bg-red-500 px-2 py-1 rounded-xl text-white absolute top-3 right-5">
+       $ {props.price}
+      </span>
 
-            <div className=" w-full">
-            <button className="bg-red-500 rounded-b-lg text-white px-2 py-3 w-full">Add Chart</button>
-            </div>
-
-
-        </div>
-    )
-}
+      <div className=" w-full">
+        <button className="bg-red-500 rounded-b-lg text-white px-2 py-3 w-full">
+          Add Chart
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Test;
