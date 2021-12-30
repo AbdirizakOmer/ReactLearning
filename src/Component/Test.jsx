5c3fde30e8b2907dd4fbe73d843a2d4816447dd0
Abdirizak Omer <abdirizakomer188@gmail.com>
2021-12-30 18:31:16 +0300

    Change

--- src/Component/Test.jsx
+++ src/Component/Test.jsx
@@ -1,6 +1,6 @@
 const Test = (props) => {
   return (
-    <div className="my-6 x-5 border-2 px-2 py-1 max-w-sm rounded-xl shadow-md  my-4 text-center flex flex-col   relative">
+    <div className="my-5 mx-auto xy-5 border-2 px-1 py-1 max-w-sm rounded-xl shadow-md  my-4 text-center flex flex-col items-center  relative">
       <div>
         <img src={props.ImageUrl} alt="" className="h-auto w-60" />
         <h1 className="font-bold text-2xl text-black">{props.name}</h1>
@@ -11,7 +11,7 @@ const Test = (props) => {
       </div>
       <div></div>
       <span className="bg-red-500 px-2 py-1 rounded-xl text-white absolute top-3 right-5">
-       $ {props.price}
+        ${props.price}
       </span>
 
       <div className=" w-full">
